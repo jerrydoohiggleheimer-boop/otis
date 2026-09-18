c647bbe17b668a5028b6f9b9e8a3ba1c38d9c050
jerrydoohiggleheimer-boop <jerrydoohiggleheimer@gmail.com>
2026-09-18 10:42:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("My JavaScript file is connected.");
+console.log("My name is Ryan.");
+console.log("I am learning JavaScript.");
+console.log("This code is running in the browser.");
+console.log("Hello Maddox");
+console.log("Welcome to JavaScript Ben.");
